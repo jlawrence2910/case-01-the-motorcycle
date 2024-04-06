86d555842006aa2884d57c355cbeda2375a0669a
jlawrence2910 <166222267+jlawrence2910@users.noreply.github.com>
2024-04-06 15:24:37 +0200

    added sonar and more movements

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-def on_button_pressed_a():
+def move_backward():
     wuKong.set_all_motor(-100, -100)
     
-def on_button_pressed_ab():
+def stop():
     wuKong.stop_all_motor()
 
 def move_forward():
     wuKong.set_all_motor(100, 100)
+
+def turn(direction):
+    if direction == 'left':
+        wuKong.set_all_motor(100, 20)
+    elif direction == 'right':
+        wuKong.set_all_motor(20, 100)
+
+
+def getSonar():
+    distance = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
+    return distance
+
 
 def on_received_string(receivedString):
     if receivedString == "forward":
@@ -13,22 +25,32 @@ def on_received_string(receivedString):
         move_forward()
     elif receivedString == "backward":
         basic.show_string('B')
+        move_backward()
     elif receivedString == "stop":
         basic.show_string('S')
+        stop()
     elif receivedString == "left":
         basic.show_string('L')
+        turn('left')
     elif receivedString == "right":
         basic.show_string('R')
+        turn('right')
 radio.on_received_string(on_received_string)
+
 
 radio.set_group(1)
 
+
 def on_forever():
     if input.button_is_pressed(Button.A):
-        on_button_pressed_a()
+        move_backward()
     elif input.button_is_pressed(Button.AB):
-        on_button_pressed_ab()
+        stop()
     elif input.button_is_pressed(Button.B):
         move_forward()
-        
+    
+    # get sonar value
+    #distance = getSonar()
+    #if distance < 2:
+    #    stop()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,13 +1,27 @@
-function on_button_pressed_a() {
+function move_backward() {
     wuKong.setAllMotor(-100, -100)
 }
 
-function on_button_pressed_ab() {
+function stop() {
     wuKong.stopAllMotor()
 }
 
 function move_forward() {
     wuKong.setAllMotor(100, 100)
+}
+
+function turn(direction: string) {
+    if (direction == "left") {
+        wuKong.setAllMotor(100, 20)
+    } else if (direction == "right") {
+        wuKong.setAllMotor(20, 100)
+    }
+    
+}
+
+function getSonar(): number {
+    let distance = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
+    return distance
 }
 
 radio.onReceivedString(function on_received_string(receivedString: string) {
@@ -16,21 +30,29 @@ radio.onReceivedString(function on_received_string(receivedString: string) {
         move_forward()
     } else if (receivedString == "backward") {
         basic.showString("B")
+        move_backward()
     } else if (receivedString == "stop") {
         basic.showString("S")
+        stop()
     } else if (receivedString == "left") {
         basic.showString("L")
+        turn("left")
     } else if (receivedString == "right") {
         basic.showString("R")
+        turn("right")
     }
     
 })
 radio.setGroup(1)
+//  get sonar value
+// distance = getSonar()
+// if distance < 2:
+//     stop()
 basic.forever(function on_forever() {
     if (input.buttonIsPressed(Button.A)) {
-        on_button_pressed_a()
+        move_backward()
     } else if (input.buttonIsPressed(Button.AB)) {
-        on_button_pressed_ab()
+        stop()
     } else if (input.buttonIsPressed(Button.B)) {
         move_forward()
     }
